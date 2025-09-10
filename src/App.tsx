eff30461990d39844724c4303beccc454f67e8da
snows77 <116312443+snows77@users.noreply.github.com>
2025-09-10 07:42:22 -0300

    header finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import styles from "./app.module.css"
+
 import { Header } from "./components/Header"
 
 export default function App() {
+  function handleRestartGame() {
+    alert("Reiniciar jogo!")
+  }
+
   return (
-    <Header />
+    <div className={styles.container}>
+      <main>
+        <Header current={5} max={10} onRestart={handleRestartGame} />
+      </main>
+    </div>
   )
 }
