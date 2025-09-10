2836bb9b41a967b8f4c6309d49b800bc7dda0575
snows77 <116312443+snows77@users.noreply.github.com>
2025-09-10 08:20:09 -0300

    Criando componente de dica

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import styles from "./app.module.css"
 
 import { Header } from "./components/Header"
+import { Tip } from "./components/Tip"
 
 export default function App() {
   function handleRestartGame() {
@@ -11,6 +12,7 @@ export default function App() {
     <div className={styles.container}>
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Tip tip="Linguagem de programação dinâmica"/>
       </main>
     </div>
   )
