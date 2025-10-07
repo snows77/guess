e7f79a68c189f2371f2b6051413c04511fdc2e02
snows77 <116312443+snows77@users.noreply.github.com>
2025-10-07 19:56:29 -0300

    main, input e button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import styles from "./app.module.css"
 
 import { Header } from "./components/Header"
+import { Input } from "./components/Input"
+import { Letter } from "./components/Letter"
 import { Tip } from "./components/Tip"
+import { Button } from "./components/Button"
 
 export default function App() {
   function handleRestartGame() {
@@ -12,7 +15,23 @@ export default function App() {
     <div className={styles.container}>
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame} />
+
         <Tip tip="Linguagem de programação dinâmica"/>
+
+        <div className={styles.word}>
+          <Letter value= "R"/>
+          <Letter value= "E"/>
+          <Letter value= "A"/>
+          <Letter value= "C"/>
+          <Letter value= "T"/>
+        </div>
+
+        <h4>Palpite</h4>
+
+        <div className={styles.guess}>
+          <Input autoFocus maxLength={1} placeholder="?"/>
+          <Button title="Confirmar" />
+        </div>
       </main>
     </div>
   )
